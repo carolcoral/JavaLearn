6a2b0e0c704a0731f95ed3735414332ae05fa638
CarolLiu <carolcoral@users.noreply.github.com>
2017-12-13 11:49:44 +0800

    创建一个判断年龄的异常

---
+++ JavaSE/Student/AgeException.java
@@ -0,0 +1,15 @@
+public class AgeException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public AgeException(){
+		super();
+	}
+	
+	public AgeException(String message){
+		super(message);
+	}
+}
