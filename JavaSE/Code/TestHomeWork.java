5e34f9c8b96f48f2f668bce30f9113751664eb45
carolcoral <947752894@qq.com>
2018-01-17 14:35:23 +0800

    2018年01月17日14:35:22

--- JavaSE/Code/TestHomeWork.java
+++ JavaSE/Code/TestHomeWork.java
@@ -1,34 +1,38 @@
-package xdl.day15;
+package xdl.day13;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Scanner;
 
-public class TestHomeWork {
+public class TestHomework {
 
-	public static void main(String[] args) throws ParseException {
-		// 1.提示用户按照指定的格式输入生日信息，计算距离1970年1月1日的天数并打印出来
-		// 如输入格式：1998年1月5日
-		Scanner sc = new Scanner(System.in);
-		System.out.println("请按照格式输入您的生日信息(yyyy年MM月dd日)：");
-		String input = sc.next();
-//		String regx = "([0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]{1}|[0-9]{1}[1-9][0-9]{2}|[1-9][0-9]{3})年(((0[13578]|1[02])月(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)月(0[1-9]|[12][0-9]|30))|(02-(0[1-9]|[1][0-9]|2[0-8]))日)";
-//		boolean re = input.matches(regx);
-		boolean re = true;
-		if (re == false) {
-			System.out.println("您输入的日期格式不正确！请重新输入！");
-		} else {
-			SimpleDateFormat stf = new SimpleDateFormat("yyyy年MM月dd日");
-			Date sr = stf.parse(input);
-			long res1 = sr.getTime();
-			// 因为当前地区处于东八区，多余8小时，因此需要减去8小时的毫秒数，然后除去一天的毫秒数
-			// 因为当前一天减去的数量没有计算前面一天，因此需要在总数上加1
-			long res = (res1 - 8 * 60 * 1000) / (24 * 60 * 60 * 1000) ;
-
-			System.out.println("您输入的生日日期已经距离1970年1月1日共：" + res + "天！！");
+	public static void main(String[] args) {
+		// 笔试题：“abcdef1234@#$%”统计该字符串中字母、数字、字符的个数
+		Scanner scanner = new Scanner(System.in);
+		// String num = new String("abcdef1234@#$%");
+		System.out.print("请输入一串字符：");
+		String num = scanner.next();
+		int[] arr = new int[3];
+		for (int i = 0; i < num.length(); i++) {
+			int re = num.charAt(i);
+			if ((re >= 'a' && re <= 'z') || (re >= 'A' && re <= 'Z')) {
+				arr[0]++;
+			} else if (re >= '0' && re <= '9') {
+				arr[1]++;
+			} else {
+				arr[2]++;
+			}
 		}
-		sc.close();
+		scanner.close();
+		System.out.println("输入了“" + num + "”这个字符串，该字符串一共有" + arr[0] + "个字母和" + arr[1] + "数字以及" + arr[2] + "个其他符号");
+
+		System.out.println("-------------------------------------------------");
+		// 笔试题：自定义成员方法将字符串“123456”转换成整数123456并打印出来
+		String pr = new String("123456");
+		int sum = 0;
+		for (int j = 0; j < pr.length(); j++) {
+			// int num2 = pr.charAt(j) - '0';
+			sum = sum * 10 + pr.charAt(j) - '0';
+		}
+		System.out.println(sum);
 	}
 
 }
