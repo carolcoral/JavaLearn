929ed0833a7c02ba8fb74194e6b90082e0b324c7
carolcoral <947752894@qq.com>
2018-05-06 22:11:22 +0800

    ActiveMQ Demo

---
+++ Finally/Code/ActiveMQ/src/com/xdl/mq/consumer/queue/QueueReceiver2.java
@@ -0,0 +1,29 @@
+
+package com.xdl.mq.consumer.queue;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 
+ * @author liang
+ * @description  队列消息监听器
+ * 
+ */
+@Component
+public class QueueReceiver2 implements MessageListener {
+
+	@Override
+	public void onMessage(Message message) {
+		try {
+			System.out.println("QueueReceiver2接收到消息:"+((TextMessage)message).getText());
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
